c80054b9c3a8a664c5a1bb49065392b19bb6f3ff
Dima163163 <khlopovdima@gmail.com>
2023-12-18 14:37:31 +0400

    filter

--- src/filter.js
+++ src/filter.js
@@ -1,6 +1,4 @@
 const filter = (arr, key, val) => {
-  console.log('val', val);
-
   const filterArr = arr.filter(value => {
     if (Object.keys(value).includes(key) && Object.values(value).includes(val)) {
       return value
